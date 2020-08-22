388981d60a0805b7cbea7f481c00bb64437bdc35
Jeremie Madara <jeremiemadara@gmail.com>
2020-08-22 11:27:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import NavBar from "./components/layout/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Jeremie Valili</h1>
+    </div>
+  );
+}
+
+export default App;
